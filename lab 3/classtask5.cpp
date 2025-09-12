9d0efbaf791221444eea7b8b37641b697293b8ed
Muhammad Adil Saeed <masadil942@gmail.com>
2025-09-12 03:12:29 -0700

    Add files via upload

---
+++ lab 3/classtask5.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Node {
+    string song;
+    Node* next;
+    Node(string s) : song(s), next(nullptr) {}
+};
+
+class Playlist {
+    Node* tail; 
+public:
+    Playlist() : tail(nullptr) {}
+
+    void addSong(const string& song) {
+        Node* newNode = new Node(song);
+        if (!tail) {
+            tail = newNode;
+            tail->next = tail;
+        } else {
+            newNode->next = tail->next;
+            tail->next = newNode;
+            tail = newNode; 
+        }
+    }
+
+    void display() {
+        if (!tail) {
+            cout << "Playlist is empty.\n";
+            return;
+        }
+        Node* temp = tail->next;
+        cout << "Playlist songs:\n";
+        do {
+            cout << "- " << temp->song << endl;
+            temp = temp->next;
+        } while (temp != tail->next);
+    }
+
+    void playSongs(int n) {
+        if (!tail) {
+            cout << "Playlist is empty, no songs to play.\n";
+            return;
+        }
+        Node* current = tail->next;
+        cout << "Playing songs:\n";
+        for (int i = 0; i < n; ++i) {
+            cout << current->song << endl;
+            current = current->next;
+        }
+    }
+
+    ~Playlist() {
+        if (!tail) return;
+        Node* current = tail->next;
+        tail->next = nullptr; 
+
+        while (current) {
+            Node* temp = current;
+            current = current->next;
+            delete temp;
+        }
+    }
+};
+
+int main() {
+    Playlist playlist;
+    playlist.addSong("Song A");
+    playlist.addSong("Song B");
+    playlist.addSong("Song C");
+    playlist.addSong("Song D");
+
+    playlist.display();
+
+    playlist.playSongs(10);
+
+    return 0;
+}
